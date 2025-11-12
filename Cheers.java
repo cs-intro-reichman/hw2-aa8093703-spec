c9f29e1fd8e65fb83d8b9f13e5d669fcf48425ea
Ariel Wyrobnk <Ariel.Wyrobnik@post.runi.ac.il>
2025-11-12 14:30:04 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,23 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String Word = args[0];
+            Word = Word.toUpperCase();
+            int number = Integer.parseInt(args[1]);
+
+            int length = Word.length();
+            String anLetters = "AEFHILMNORSX";
+
+
+            for (int i = 0; i < length; i++ ){
+                if (anLetters.indexOf(Word.charAt(i)) == -1) { System.out.println("Give me a  " + Word.charAt(i) + ": " + Word.charAt(i) + "!");
+                        
+                } else System.out.println("Give me an " + Word.charAt(i) + ": " + Word.charAt(i) + "!");
+            }
+
+            System.out.println("What does that spell?");
+
+            for (int i = 0; i < number; i++){
+                System.out.println(Word + "!!!");
+            }
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,58 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+	    String inputLetter = args[1];
+		inputLetter.equals("v");
+
+		int Input = Integer.parseInt(args[0]);
+
+		int count = 1;
+
+		if (inputLetter.equals("v")) {
+
+			for (int M = 1; M <= Input; M++){
+
+		int N = M;
+		count = 1;
+				
+		do {
+			System.out.print(N + " ");
+			if (N % 2 == 0) { N = N / 2;
+				
+			} else N = N * 3 + 1;
+
+			count++;
+		}
+
+		while (N != 1);
+
+		System.out.println(N + " (" + count +  ")");
+			
+			}
+		System.out.println("");	
+		System.out.println("Every one of the first " + Input + " hailstone sequences reached 1.");
+			
+		}
+
+		else if (inputLetter.equals("c")) {
+			for (int M = 1; M <= Input; M++){
+
+			int N = M;
+			count = 1;
+				
+			do {
+				if (N % 2 == 0) { N = N / 2;
+				
+				} else N = N * 3 + 1;
+
+				count++;
+				}
+
+			while (N != 1);
+		}
+
+		System.out.println("Every one of the first " + Input + " hailstone sequences reached 1.");
 	}
+}
 }
